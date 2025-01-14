7e702c2b5fe3aabcf4deb932afd55166e1056de4
alex <alex@truthidea.org>
2025-01-14 18:36:16 +0800

    更新rclone版本

--- Formula/rclone.rb
+++ Formula/rclone.rb
@@ -1,21 +1,21 @@
 class Rclone < Formula
   desc "Rsync for cloud storage"
   homepage "https://rclone.org/"
-  version "1.68.1"
+  version "1.69.0"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/rclone/rclone/releases/download/v1.68.1/rclone-v1.68.1-osx-amd64.zip"
-      sha256 "d81e091571bc040282dab39d0cdee6b3215774de389469cd850c9803d6191de3"
+      url "https://github.com/rclone/rclone/releases/download/v1.69.0/rclone-v1.69.0-osx-amd64.zip"
+      sha256 "88475be8666944d5161bd15605d483f1bb55d9a46220b9dcf317b26e01659009"
 
       def install
         bin.install "rclone"
       end
     end
     on_arm do
-      url "https://github.com/rclone/rclone/releases/download/v1.68.1/rclone-v1.68.1-osx-arm64.zip"
-      sha256 "38e7588e13c93ee710daa44bb798437eca7fd583d4e98d0bce6c685ff11f4152"
+      url "https://github.com/rclone/rclone/releases/download/v1.69.0/rclone-v1.69.0-osx-arm64.zip"
+      sha256 "d8d22aca8f4af4c24ea01668a452d02888bef45e3a01c663cbb7d78bd1e21db2"
 
       def install
         bin.install "rclone"
